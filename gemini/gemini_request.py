53edb5c01cf6ca2192964c0d36f3cda9e143b64a
amelieangelina <amelie.kammerer01@gmail.com>
2024-07-31 08:14:48 +0200

    chore

--- gemini/gemini_request.py
+++ gemini/gemini_request.py
@@ -13,12 +13,6 @@ load_dotenv(env_path)
 genai.configure(api_key=os.environ.get("API_KEY"))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-
-
-# response = model.generate_content("What do you know about the trash system in Europe?")
-# print(response.text)
-# print(response)
-
 def detect_trash():
     img = Image.open("2024-07-30-102827.jpg")
     response = model.generate_content(["What kind of trash is displayed in the picture. What material is it? ", img], stream=True)
