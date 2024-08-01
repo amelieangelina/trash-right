93af5cd6725cfb8f0f5aa8bbafd670ecf7568829
amelieangelina <amelie.kammerer01@gmail.com>
2024-08-01 12:49:53 +0200

    python package setup

---
+++ gemini/__init__.py
@@ -0,0 +1,1 @@
+from .gemini_request import detect_trash
---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='python_package',
+    version='0.1',
+    packages=find_packages(),
+    python_requires='>=3.12.3',
+)
