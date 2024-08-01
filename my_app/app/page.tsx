1a4cbfff2252ac473fd3bce69363ed40390b5033
amelieangelina <amelie.kammerer01@gmail.com>
2024-08-01 12:21:29 +0200

    center landing page

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from 'next/link';
 import { useEffect, useState, useRef } from 'react';
 
+
 export default function Home() {
 
   const [isPhotoTaken, setIsPhotoTaken] = useState(false);
@@ -139,26 +140,26 @@ export default function Home() {
       </div>
 
       
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {/* TODO: replace page  */}
-        <Link
-          href="/landing"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-          replace={true}
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Landing Page{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Original landing page of Nextjs to find templates and documentation.
-          </p>
-        </Link>
+      <div className="flex justify-center items-center">
+          {/* TODO: replace page */}
+          <Link
+            href="/landing"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+            replace={true}
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Landing Page{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              Original landing page of Nextjs to find templates and documentation.
+            </p>
+          </Link>
+        
       </div>
 
     </main>
